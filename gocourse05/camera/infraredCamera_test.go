404c8dfd842938055b67d06879246e619ae44de8
Tkach Viktoriia <viktoriia.tkach@sporthub.tech>
2024-08-25 21:11:14 +0300

    HW-05: Added tests

--- gocourse05/camera/infraredCamera_test.go
+++ gocourse05/camera/infraredCamera_test.go
@@ -1,1 +1,33 @@
 package camera
+
+import (
+	"testing"
+)
+
+func TestInfraredCameraRetrieveData(t *testing.T) {
+	t.Run("Found", func(t *testing.T) {
+		c := NewInfraredCamera(1, "Canon", []Data{{1, "Коза", "Побігла вліво"}})
+		data, _ := c.retrieveData()
+
+		if len(*data) == 0 {
+			t.Errorf("retrieveData() should not return empty data.")
+		}
+
+		for _, d := range *data {
+			if d.Animal != "Коза" || d.Movement != "Побігла вліво" {
+				t.Errorf("retrieveData() = %+v", d)
+			}
+		}
+	})
+
+	t.Run("Not found", func(t *testing.T) {
+		c := NewInfraredCamera(1, "Canon", []Data{})
+		_, err := c.retrieveData()
+		if err == nil {
+			t.Errorf("retrieveData() should return error = %v", err)
+		}
+	})
+}
+
+func TestInfraredCameraProcessData(t *testing.T) {
+}
--- gocourse05/server_test.go
+++ gocourse05/server_test.go
@@ -1,1 +1,82 @@
 package main
+
+import (
+	"testing"
+	"time"
+
+	"gocourse05/camera"
+)
+
+func TestSaveProcessedData(t *testing.T) {
+	var testMemory Memory
+	type args struct {
+		p camera.Processor
+		m *Memory
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Success saveProcessedData",
+			args: args{
+				p: camera.NewDayCamera(1, "test", []camera.Data{{1, "Коза", "Побігла вліво"}}),
+				m: &testMemory,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Fail saveProcessedData",
+			args: args{
+				p: camera.NewDayCamera(1, "test", []camera.Data{}),
+				m: &testMemory,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := saveProcessedData(tt.args.p, tt.args.m); (err != nil) != tt.wantErr {
+				t.Errorf("saveProcessedData() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestSendProcessedData(t *testing.T) {
+	type args struct {
+		m   Memory
+		url string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Success sendProcessedData",
+			args: args{
+				m:   []camera.ProcessedData{camera.NewProcessedData(time.Now(), "Медвідь, побіг вліво")},
+				url: "test",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Fail sendProcessedData",
+			args: args{
+				m:   []camera.ProcessedData{camera.NewProcessedData(time.Now(), "")},
+				url: "test",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := sendProcessedData(tt.args.m, tt.args.url); (err != nil) != tt.wantErr {
+				t.Errorf("sendProcessedData() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
