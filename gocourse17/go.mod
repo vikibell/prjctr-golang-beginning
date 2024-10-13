872407312d27117442964a2d6e4c0d8ccba1514f
Tkach Viktoriia <viktoriia.tkach@sporthub.tech>
2024-10-13 20:31:40 +0300

    HW-17: Added test on review

--- gocourse17/go.mod
+++ gocourse17/go.mod
@@ -3,6 +3,7 @@ module github.com/vikibell/prjctr-golang-beginning/gocourse17
 go 1.23.0
 
 require (
+	github.com/google/go-cmp v0.6.0
 	google.golang.org/grpc v1.67.1
 	google.golang.org/protobuf v1.35.1
 )
--- gocourse17/service/review_test.go
+++ gocourse17/service/review_test.go
@@ -1,1 +1,65 @@
 package service
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"reflect"
+	"testing"
+)
+
+func TestNewReview(t *testing.T) {
+	cs := int32(2)
+	sq := int32(3)
+	fs := int32(4)
+
+	review := NewReview(cs, sq, fs)
+	want := Review{
+		CargoState:       cs,
+		ServiceQuality:   sq,
+		FulfillmentSpeed: fs,
+	}
+
+	if got := review; !reflect.DeepEqual(got, want) {
+		t.Errorf("NewReview(): got = %v, want %v", got, want)
+	}
+}
+
+func TestIsValidRating(t *testing.T) {
+	cs := int32(2)
+	sq := int32(6)
+
+	t.Run("Valid", func(t *testing.T) {
+		if got := IsValidRating(cs); !cmp.Equal(got, true) {
+			t.Errorf("NewReview(): got = %v, want %v", got, true)
+		}
+	})
+	t.Run("Not valid", func(t *testing.T) {
+		if got := IsValidRating(sq); !cmp.Equal(got, false) {
+			t.Errorf("NewReview(): got = %v, want %v", got, false)
+		}
+	})
+}
+
+func TestReviewHistory_AddGet(t *testing.T) {
+	cs := int32(2)
+	sq := int32(3)
+	fs := int32(4)
+	driverId := int32(1)
+
+	review := NewReview(cs, sq, fs)
+	history := NewReviewHistory()
+	history.AddReview(driverId, review)
+
+	got, _ := history.GerReviews(driverId)
+
+	want := []Review{
+		{
+			CargoState:       cs,
+			ServiceQuality:   sq,
+			FulfillmentSpeed: fs,
+		},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("ReviewHistory.Add(): got = %v, want %v", got, want)
+	}
+}
